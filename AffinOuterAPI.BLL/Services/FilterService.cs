5850a6f2f9ad1de09fe6e4f79a3fda76fea3af7d
Sergei <kostricasergei323@gmail.com>
2021-05-09 20:21:42 +0300

    Fixed bugs

--- AffinOuterAPI.BLL/Services/FilterService.cs
+++ AffinOuterAPI.BLL/Services/FilterService.cs
@@ -15,6 +15,7 @@ namespace AffinOuterAPI.BLL.Services
         private bool GetFilterQuery(string codeName, string criterias, out string filterQuery)
         {
             filterQuery = string.Empty;
+         
             if (criterias.Contains("|"))
             {
                 List<string> criteriasFilter = new List<string>();
@@ -24,22 +25,21 @@ namespace AffinOuterAPI.BLL.Services
                 {
                     if (criteria.Contains("&"))
                     {
-                        criteriasFilter.Add($"({string.Join(" AND ", criteria.Split('&').Select(x => $"\"{x}\""))})");
+                        criteriasFilter.Add($"({string.Join(" AND ", criteria.Split('&').Select(x => x.Contains(" ") ? $"\"{x}\"" : x))})");
                     }
-                    else criteriasFilter.Add($"\"{criteria}\"");
+                    else criteriasFilter.Add(criteria.Contains(" ") ? $"\"{criteria}\"" : criteria);
                 }
-
                 filterQuery = $"{codeName}:({string.Join(" OR ", criteriasFilter)})";
             }
             else if (criterias.Contains("&"))
             {
-                filterQuery = $"{codeName}:({string.Join(" AND ", criterias.Split('&').Select(x => $"\"{x}\""))})";
+                filterQuery = $"{codeName}:({string.Join(" AND ", criterias.Split('&').Select(x => x.Contains(" ") ? $"\"{x}\"" : x))})";
             }
             else
             {
-                filterQuery = $"{codeName}:(\"{criterias}\")";
+                filterQuery = $"{codeName}:({(criterias.Contains(" ") ? $"\"{criterias}\"" : criterias)})";
             }
-
+            
             return !string.IsNullOrEmpty(filterQuery);
         }
 
@@ -49,17 +49,18 @@ namespace AffinOuterAPI.BLL.Services
             (string titles, string authors,
                 string publishers, string languages, string years) = _currentFilter;
 
+            titles = string.IsNullOrEmpty(titles) ? coreRequest : $"{coreRequest}|{titles}";
             List<string> filterQuery = new List<string>();
-            foreach(KeyValuePair<string, string> codeCriteria in new Dictionary<string, string>
+            foreach (KeyValuePair<string, string> codeCriteria in new Dictionary<string, string>
             {
-                {"title", $"{coreRequest}|{titles}"},
+                {"title", titles},
                 {"authors", authors},
                 {"publisher", publishers},
                 {"language.name", languages},
                 {"year", years}
             })
             {
-                if(!string.IsNullOrEmpty(codeCriteria.Key) && 
+                if (!string.IsNullOrEmpty(codeCriteria.Key) &&
                     !string.IsNullOrEmpty(codeCriteria.Value) &&
                     GetFilterQuery(codeCriteria.Key, codeCriteria.Value, out string filter))
                 {
